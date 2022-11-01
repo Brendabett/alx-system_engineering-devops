5a8cb2d1401bd5316cd0ddc1a61b5f51cfa47a8b
Brendabett <brendabett33@gmail.com>
2022-11-01 04:39:11 -0700

    My commit

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-# Fixes Apache 500 error
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+# Puppet script that fixes whole typo errors in php files
+exec { '/var/www/html/wp-setting.php':
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  command => sed -i s/.phpp/.php/g /var/www/html/wp-settings.php,
 }
